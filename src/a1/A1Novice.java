e4186586612bc2a0eee21e1d34b2b19b9f5aeb4b
Fayyaz01 <FayyazSulman@gmail.com>
2019-08-29 14:21:50 -0400

    reformatted

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -2,190 +2,109 @@ package a1;
 
 import java.util.Scanner;
 
+
+
 public class A1Novice {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		
 
-		// Your code follows here.
-		
+			System.out.println("START HERE");
+			// count for the number of customers
+			
+			int count = scan.nextInt();
 
-		
-		// System.out.println("Print the number of customers");
-		
-		int count = scan.nextInt();
-		
-		Customer[] allCust = new Customer[count];
-		
-		// System.out.println(allCust.length + "cow");
-		
-		for (int i = 0; i < count; i++) {
-		// 	System.out.println("Print Customer Info \n");
 			
-			allCust[i] = customerGenerator();
+			//array of said customers
+			Customer[] allCust = new Customer[count];
 			
-		// 	System.out.println("First Name: " + allCust[i].fName + " Last Name: " + allCust[i].lName + " Order: " + allCust[i].order);
-			
-			Item[] cart = new Item[allCust[i].order];
-			
-			for (int j = 0; j < allCust[i].order; j++) {
+			for (int i = 0; i < count; i++) {
+				allCust[i] = customerGenerator();
 				
-				cart[j] = itemCreator();
+				// generate cart and it's price total
+				Item[] cart = new Item[allCust[i].order];
 				
-				
+				for (int j = 0; j < allCust[i].order; j++) {
+//					allCust[i].cart[j] = itemGenerator();
+					cart[j] = itemGenerator();
+				}
+				allCust[i].cart = cart;
+				double cartTotal = cartPrice(cart);
+				allCust[i].total = cartTotal;
 				
 			}
 			
-			double total = cartPrice(cart);
-			allCust[i].total = total;
+			for (int q = 0; q < count; q++) {
+				Customer currCust = allCust[q];
+				System.out.println(currCust.fName + ". " + currCust.lName + ": " + currCust.total);
+			}
 			
+			System.out.println("Customer Info: " + allCust[0].fName + allCust[0].fNameFull);
 			
-		 //	System.out.println("The total for customer is " + total);
-		}
 
-		for (int q = 0; q < allCust.length; q++) {
-			System.out.println(allCust[q].fName + ". " + allCust[q].lName + ": " + allCust[q].total);
-		}
 		
-//		Customer biggestD = Biggest(allCust);
-//		
-//		Customer smallestD = Smallest(allCust);
-//		
-//		double average = Average(allCust);
-//		
-//		
-//		System.out.println("Biggest: " + biggestD.fullName + " " + biggestD.lName + " (" + biggestD.total + ")");
-//		System.out.println("Smallest:" + smallestD.fullName + " " + smallestD.lName + " (" + smallestD.total + ")");
-//		System.out.println("Average: " + average);
-//		
-	
 		
 	}
+	
+
 	
 	static Customer customerGenerator() {
 		Scanner scan = new Scanner(System.in);
 		
-		// scan.close();
+
+		String fNameFull = scan.next();
+		String lName = scan.next();
+		int order = scan.nextInt();
 		
-		String[] input = new String[3];
+
 		
-		input = scan.nextLine().split(" ");
-		// char fName = input[0].charAt(0);
-		String fName = input[0];
-		String lName = input[1];
-		String order = input[2];
-		int cat = Integer.parseInt(order);
-		
-		Customer aCust = new Customer(fName, lName, cat);
+		Customer newCust = new Customer(fNameFull, lName, order);
 		
 		
-		return aCust;
+		return newCust;
 		
 	}
 	
-	static double cartPrice(Item[] a) {
-		double total = 0;
-		
-		for (int i = 0; i < a.length; i++) {
-			total += a[i].total;
-//			System.out.println(total);
-			
-			
-		}
-		
-		return total;
-		
-	
-		
-		
-		
-		
-	}
-	
-	
-	static Item itemCreator() {
+	static Item itemGenerator() {
 		Scanner scan = new Scanner(System.in);
-		// scan.close();
 		
-		String[] input = new String[3];
-		input = scan.nextLine().split(" ");
+		int quantity = scan.nextInt();
+		String name = scan.next();
+		double price = scan.nextDouble();
 		
-		String fquant = input[0];
-		double quant = Integer.parseInt(fquant);
+		Item newItem = new Item(quantity, name, price);
 		
-		String fname = input[1];
-		
-		
-		String fcost = input[2];
-		double cost = Double.parseDouble(fcost);
-		
-		Item newItem = new Item(quant, "", cost);
 		
 		return newItem;
 		
 		
-	}
-	
-	static Customer Biggest(Customer[] a) {
-		Customer biggestD = a[0];
-		
-		for (int i = 0; i < a.length; i++) {
-			if (a[i].total > biggestD.total) {
-				biggestD = a[i];
-			}
-		}
-		
-		return biggestD;
 		
 	}
 	
-	static Customer Smallest(Customer[] a) {
-		Customer smallestD = a[0];
+	static double cartPrice(Item[] a) {
 		
-		for (int i = 0; i < a.length; i++) {
-			if (a[i].total < smallestD.total) {
-				smallestD = a[i];
-			}
-		}
-		
-		return smallestD;
-		
-	}
-	
-	static double Average(Customer[] a) {
-		double avg = 0;
-		double total = 0;
-		
+		double total = 0.00;
 		for (int i = 0; i < a.length; i++) {
 			total += a[i].total;
 		}
-		avg = total / a.length;
 		
-		return avg;
-		
-		
-		
+		return total;
+	
 	}
 	
-	
-	
-	
-	// adds up everything in the cart
-	
-
-	
-	public static class Customer {
+public static class Customer {
 		
 		char fName;
+		String fNameFull;
 		String fullName;
 		String lName;
 		int order;
 		double total;
-		Item[] purchased;
+		Item[] cart;
 		
 		Customer(String a, String b, int c) {
+			this.fNameFull = a;
 			this.fName = a.charAt(0);
 			this.lName = b;
 			this.order = c;
@@ -214,6 +133,7 @@ public class A1Novice {
 		
 		
 	}
+	
 	
 	
 }
